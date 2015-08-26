e326dfbb59fc397626cca4e209ca180aa006595a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 12:19:20 +0300

    Add package details

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "Create a role, checking first if the role name is already defined.",
+  version: "0.1.0",
+  name: "brylie:create-role-if-undefined",
+  git: "https://github.com/brylie/meteor-create-role-if-undefined.git"
+});
+
+Package.on_use(function (api, where) {
+  // Meteor version
+  api.versionsFrom("1.0.1");
+  
+  // Dependencies
+  api.use("alanning:roles@1.2.11", "server");
+
+  // Files
+  api.add_files("server/methods/roles.js", "server");
+});
